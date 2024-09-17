be2af7f87b9422885f34065430e463f20cd3bdf2
lav333 <val.dragasevic@gmail.com>
2024-09-17 22:40:42 +0300

    Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = ""
+    if (num >= 0) {
+        for (let i=0; i<num; i++) {
+            result += string
+        }
+        return result
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
