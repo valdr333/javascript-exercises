3e5d9d9959314e5bfbcce71785d5acbc60cf835a
lav333 <val.dragasevic@gmail.com>
2024-09-17 23:05:52 +0300

    Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = '';
+    let array = string.split('');
+    let i=(array.length-1);
+    for (i; i>=0; i--) {
+        result += array[i];
+    }
+    return result
 };
 
 // Do not edit below this line
