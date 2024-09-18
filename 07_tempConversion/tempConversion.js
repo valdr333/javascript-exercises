520c32e796077a11e9c81473c62ec53abc462656
lav333 <val.dragasevic@gmail.com>
2024-09-18 09:09:54 +0300

    Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * (5/9);
+  return Number(tempC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC * (9/5) + 32);
+  return Number(tempF.toFixed(1));
 };
 
 // Do not edit below this line
