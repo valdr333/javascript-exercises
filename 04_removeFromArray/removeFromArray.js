8400dd2189c4536aa023389d8daf6b2f7d0c31b2
lav333 <val.dragasevic@gmail.com>
2024-09-18 08:28:30 +0300

    Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    for (let i=1; i<arguments.length; i++) {
+        let count = 0;
+        for (let j=0; j<array.length; j++) {
+            if (array[j] == arguments[i]) {
+                count += 1
+            }
+        }
+        for (; count>0; count--) {
+            let index = array.indexOf(arguments[i]);
+            let newArray = array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
