ac38b2a6a0213b0c2d79ab210f699295e7f31e3c
lav333 <val.dragasevic@gmail.com>
2024-09-18 08:39:07 +0300

    Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0
+    if ((num1>0 && num2>0) && (Number.isInteger(num1) && Number.isInteger(num2))) {
+        for (let i=Math.min(num1, num2); i<=Math.max(num1, num2); i++) {
+            sum += i;
+        }
+        return sum
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
