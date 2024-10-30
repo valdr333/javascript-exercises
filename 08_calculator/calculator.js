3dc0467ff37dc027fe50c8c74a7f577ba9615d2b
lav333 <val.dragasevic@gmail.com>
2024-10-31 00:39:27 +0100

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(A) {
+  let suma = 0
+	A.forEach(br => suma += br);
+  return suma;
 };
 
-const multiply = function() {
-
+const multiply = function(A) {
+  let rez = A.reduce((um, curr) => um * curr, 1);
+  return rez;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let suma = 1;
+  for (let i = a; i > 0; i--) {
+    suma *= i;
+  }
+  return suma;
 };
 
 // Do not edit below this line
