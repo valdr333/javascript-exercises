4958cd8292ce54cd4e02dbf71c9f8a9ce42244f9
lav333 <val.dragasevic@gmail.com>
2024-11-02 15:36:18 +0100

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let arr = word.split('');
+    let alph = "abcdefghijklmnopqrstuvwxyz123456789";
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == "number") {
+            newArr.push(arr[i]);
+        } else if (alph.includes(arr[i].toLowerCase())) {
+            newArr.push(arr[i].toLowerCase());
+        }
+    }
+    let newWord = newArr.join('');
+    let revArr = newArr.reverse();
+    let revWord = revArr.join('');
+    return newWord == revWord;
 };
 
 // Do not edit below this line
