367b7c5c010e5fa43148ed2c5ae918c497715880
lav333 <val.dragasevic@gmail.com>
2024-11-02 15:43:20 +0100

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let arr = [0, 1, 1];
+    if (n >= 0) {
+        for (let i = 3; i <= n; i++) {
+            arr.push(arr[i-1]+arr[i-2]);
+        }
+        return arr[n];
+    } else {
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
