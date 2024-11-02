831d18b4baa3a5040be1edc9424bb1d0300b8eb2
lav333 <val.dragasevic@gmail.com>
2024-11-02 16:48:14 +0100

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let newArr = arr.map(function(person) {
+        if ("yearOfDeath" in person) {
+            return person.yearOfDeath - person.yearOfBirth;
+        } else {
+            return 2024 - person.yearOfBirth;
+        }
+    });
+    let max = 0;
+    for (let i = 1; i < newArr.length; i++) {
+        if (newArr[i] > newArr[max]) {
+            max = i;
+        }
+    }
+    return arr[max];
 };
 
 // Do not edit below this line
